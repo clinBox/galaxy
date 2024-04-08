c258847b715a53252d09339092562a4c1ce2c4b9
clinBox <132152727+clinBox@users.noreply.github.com>
2024-04-08 20:31:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let price_field = document.querySelector('.price')
+let promo_field = document.querySelector('.promo')
+let calc_btn = document.querySelector('.btn')
+let result_field = document.querySelector('.result')
+
+function check_promo(price, promo) {
+// Здесь вычисляем и возвращаем итоговую стоимость
+
+}
+
+calc_btn.addEventListener('click', function() {
+    let price = +price_field.value
+    let promo = promo_field.value
+}
